0136c8a237e172d459c6fe82f0cf565b756684fd
Dillon <dillon.hall3817@gmail.com>
2026-01-30 20:54:52 -0700

    scrolling sort of works

--- thegame/gamemain.c
+++ thegame/gamemain.c
@@ -21,6 +21,7 @@ void move_player(player_t *player);
 void main(void)
 {
   player_t player; // player object
+  scroll_t scroller;
   //Window_t window; // location of the window
 
   // init the player
@@ -31,9 +32,9 @@ void main(void)
   player.ctrlr = 0;
   player.playerSp = 0xB1;
 
-  // init the window
-  //window.windowX = 0;
-  //window.windowY = 0;
+  // init the scroller
+  scroller.sx = 0;
+  scroller.sy = 0;
 
   // write text to name table
   put_str(NTADR_A(2,0), "Nametable A, Line 0");
@@ -52,6 +53,11 @@ void main(void)
 
     // update player from the controller
     move_player(&player);
+    // figure out if we have to scroll
+    // scroll if we need to
+    //map_scroll(scroll_t *scroll, player_t *player, char ncontroller)
+    map_scroll(&scroller,&player,0);
+    scroll(scroller.sx,scroller.sy);
   }
 }
 
--- thegame/scrolling.c
+++ thegame/scrolling.c
@@ -1,4 +1,5 @@
 #include "scrolling.h"
+#include "gameconfig.h"
 #include "./../neslib/neslib.h"
 
 
@@ -11,4 +12,23 @@ void map_scroll(scroll_t *scroll,player_t *player,char ncontroller)
 {
     unsigned char pad;
     pad = pad_poll(ncontroller);
+
+    // determine if the player is moving
+
+  if(pad & PAD_UP && (player->py + player->dy) > LOWER_SCROLL_LIMIT_Y)
+  {
+    scroll->sy -= 1;
+  }
+  if(pad & PAD_DOWN && (player->py - player->dy) <= UPPER_SCROLL_LIMIT_Y)
+  {
+    scroll->sy += 1;
+  }
+  if(pad & PAD_RIGHT && (player->px + player->dx) < UPPER_SCROLL_LIMIT_X)
+  {
+    scroll->sx += 1;
+  }
+  if(pad & PAD_LEFT && (player->px - player->dx) > LOWER_SCROLL_LIMIT_X)
+  {
+    scroll->sx -= 1;
+  }
 }
