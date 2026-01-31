569f56e5c0ebda95f0c41f86cf68ee237e53d8d6
Dillon <dillon.hall3817@gmail.com>
2026-01-30 23:16:12 -0700

    created some macros for game states

--- thegame/gameconfig.h
+++ thegame/gameconfig.h
@@ -1,10 +1,15 @@
 #ifndef GAMECONFIG_H
 #define GAMECONFIG_H
 #define NACTORS 1 // number of actors that we're drawing on the screen
+// X and Y denote the box that can be used to interact around the player
 // limits for Y axis
 #define LOWER_SCROLL_LIMIT_Y 20
 #define UPPER_SCROLL_LIMIT_Y 200
 // limits for the X axis
 #define LOWER_SCROLL_LIMIT_X 20
 #define UPPER_SCROLL_LIMIT_X 230
+// game states
+#define TITLESCREEN 0
+#define GAMETIME 1
+#define GAMEOVER 2
 #endif
