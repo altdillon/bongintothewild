473a567d13b98559f6bc036feab8daa811a1a0d4
Dillon <dillon.hall3817@gmail.com>
2026-01-30 18:37:04 -0700

    added a game config.h

---
+++ thegame/gameconfig.h
@@ -0,0 +1,5 @@
+#ifndef GAMECONFIG_H
+#define GAMECONFIG_H
+#define NACTORS 1 // number of actors that we're drawing on the screen
+
+#endif
--- thegame/gamemain.c
+++ thegame/gamemain.c
@@ -2,8 +2,10 @@
 // include NESLIB header
 #include "./../neslib/neslib.h"
 #include "pallets.h"
+#include "gameconfig.h"
 
 void setup_graphics();
+void draw_sprites();
 
 // main function, run after console reset
 void main(void)
