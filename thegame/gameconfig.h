ef3f4b9bb38d6b67b5db1664763c73855b49e25f
Dillon <dillon.hall3817@gmail.com>
2026-01-30 19:56:12 -0700

    player sprite movement is now limited to a defined box

--- thegame/gameconfig.h
+++ thegame/gameconfig.h
@@ -1,5 +1,10 @@
 #ifndef GAMECONFIG_H
 #define GAMECONFIG_H
 #define NACTORS 1 // number of actors that we're drawing on the screen
-
+// limits for Y axis
+#define LOWER_SCROLL_LIMIT_Y 20
+#define UPPER_SCROLL_LIMIT_Y 200
+// limits for the X axis
+#define LOWER_SCROLL_LIMIT_X 20
+#define UPPER_SCROLL_LIMIT_X 230
 #endif
--- thegame/player.c
+++ thegame/player.c
@@ -1,25 +1,26 @@
 #include "player.h"
+#include "gameconfig.h"
 #include "./../neslib/neslib.h"
 
 void move_player(player_t *player)
 {
   unsigned char pad;
-  pad = pad_poll(player->ctrlr);
-  // figure out what buttons where pressed
-  if(pad & PAD_UP)
+  pad = pad_poll(player->ctrlr); // pad poll pulls down which buttons have actually been pressed
+  
+  if(pad & PAD_UP && (player->py + player->dy) > LOWER_SCROLL_LIMIT_Y)
   {
     player->py -= player->dy;
   }
-  else if(pad & PAD_DOWN)
+  else if(pad & PAD_DOWN && (player->py - player->dy) <= UPPER_SCROLL_LIMIT_Y)
   {
     player->py += player->dy;
   }
-  else if(pad & PAD_RIGHT)
+  else if(pad & PAD_RIGHT && (player->px + player->dx) < UPPER_SCROLL_LIMIT_X)
   {
     player->px += player->dx;
   }
-  else if(pad & PAD_LEFT)
+  else if(pad & PAD_LEFT && (player->px - player->dx) > LOWER_SCROLL_LIMIT_X)
   {
-    player->px -= player->dy;
+    player->px -= player->dx;
   }
 }
