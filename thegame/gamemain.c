9d9457936fcdfec157dbbbc37e8eb56a37004ef0
Dillon <dillon.hall3817@gmail.com>
2026-01-30 18:35:27 -0700

    started working on getting a test sprite up on the screen

--- thegame/gamemain.c
+++ thegame/gamemain.c
@@ -1,30 +1,25 @@
-/*
-A simple "hello world" example.
-Set the screen background color and palette colors.
-Then write a message to the nametable.
-Finally, turn on the PPU to display video.
-*/
 
 // include NESLIB header
 #include "./../neslib/neslib.h"
+#include "pallets.h"
+
+void setup_graphics();
 
 // main function, run after console reset
 void main(void)
 {
-
-  // set palette colors
-  pal_col(0, 0x02); // set screen to dark blue
-  pal_col(1, 0x14); // fuchsia
-  pal_col(2, 0x20); // grey
-  pal_col(3, 0x30); // white
-
-  // write text to name table
-  vram_adr(NTADR_A(2, 2));        // set address
-  vram_write("HELLO WORLD!", 13); // write bytes to video RAM
-
-  // enable PPU rendering (turn on screen)
-  ppu_on_all();
+  setup_graphics();
 
   // infinite loop
   while (1);
+}
+
+void setup_graphics()
+{
+  // clear the sprites and turn on the ppu
+  oam_clear();
+  // set palette colors
+  pal_all(PALETTE);
+  // turn on PPU
+  ppu_on_all();
 }
