683043aa2d83291abff3e211859f179fb6f94433
Dillon <dillon.hall3817@gmail.com>
2026-01-30 19:17:33 -0700

    we can now move a sprite around on the screen

--- thegame/controller.h
+++ thegame/controller.h
@@ -1,15 +1,3 @@
 #ifndef CONTROLLER_H
-#define NBUTTONS 8
-#define UP 0
-#define DOWN 1
-#define LEFT 2
-#define RIGHT 3
-#define BTNA 4
-#define BTNB 5
-#define START 6
-#define SELECT 7
-
-unsigned char ctrl_left[NBUTTONS];
-unsigned char ctrl_right[NBUTTONS];
 
 #endif
--- thegame/gamemain.c
+++ thegame/gamemain.c
@@ -7,11 +7,17 @@
 
 // struct to hold some basic information about the player
 typedef struct {
+  int ctrlr; // controller that the player is driven by
+  // position vector for the player
   unsigned char px;
   unsigned char py;
+  // delta vector for the player
+  unsigned char dx;
+  unsigned char dy;
 } player_t;
 
 void setup_graphics();
+void move_player(player_t *player);
 
 // main function, run after console reset
 void main(void)
@@ -19,6 +25,9 @@ void main(void)
   player_t player;
   player.px = 120;
   player.py = 120;
+  player.dx = 3;
+  player.dy = 3;
+  player.ctrlr = 0;
 
   setup_graphics();
   while(1)
@@ -26,6 +35,32 @@ void main(void)
     oam_clear();
     oam_spr(player.px, player.py, 0x04, 0,0);
     ppu_wait_nmi();
+
+    // update player from the controller
+    move_player(&player);
+  }
+}
+
+void move_player(player_t *player)
+{
+  unsigned char pad;
+  pad = pad_poll(player->ctrlr);
+  // figure out what buttons where pressed
+  if(pad & PAD_UP)
+  {
+    player->py -= player->dy;
+  }
+  else if(pad & PAD_DOWN)
+  {
+    player->py += player->dy;
+  }
+  else if(pad & PAD_RIGHT)
+  {
+    player->px += player->dx;
+  }
+  else if(pad & PAD_LEFT)
+  {
+    player->px -= player->dy;
   }
 }
 
