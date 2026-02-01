21abc4e8651ddbcb504853581c1bd5b78ba9f3ba
Dillon <dillon.hall3817@gmail.com>
2026-02-01 11:19:31 -0700

    compute_maks now computes the location of random masks

--- thegame/mask.c
+++ thegame/mask.c
@@ -1,7 +1,13 @@
 #include "maskitem.h"
+#include "rndhelper.h"
 
 void compute_masks(mask_t *array,unsigned char nmasks)
 {
-
-
+    unsigned char i;
+    for(i=0;i<nmasks;i++)
+    {
+        // compute the random x and y values
+        array[i].x = rndint(0,127);
+        array[i].y = rndint(0,127);
+    }
 }
